27e26f8edd2d548b4b021f5de5053ef35bb1b121
Jaehwan Ryu <time0121@gmail.com>
2014-03-28 13:09:23 -0400

    Use html5 pushstate

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,7 +1,8 @@
 'use strict';
 
 angular.module('expAuth-session', ['ngRoute'])
-  .config(function ($routeProvider) {
+  .config(function ($routeProvider, $locationProvider) {
+    $locationProvider.html5Mode(true);
     $routeProvider
       .when('/', {
         templateUrl: 'views/main.html',
